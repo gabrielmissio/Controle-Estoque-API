32a8020b5ecb63757fefd8218cc599c8b261cc79
gabmissil@gmail.com <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2020-11-24 08:17:38 -0300

    definida relacao n:n entre produtos e pedido venda, gerado instansis no bd

--- src/main/java/br/com/gmissio/controleestoque/model/PedidoVenda.java
+++ src/main/java/br/com/gmissio/controleestoque/model/PedidoVenda.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
@@ -18,9 +19,9 @@ public class PedidoVenda {
 	 
 	@ManyToOne
 	private Cliente cliente;
-	@ManyToOne
+	@ManyToOne 
 	private Vendedor vendedor;
-	@OneToMany
+	@ManyToMany
 	private List<Produto> listProdutos;
 	private Double valorTotal;
 	private LocalDateTime dataPedido;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,21 +1,30 @@
 INSERT INTO CLIENTE(nome, cpf, endereco, cidade) VALUES('Carlos', '123.456.789-12', 'Rua planalto, Bariro LIMOEIRO', 'Monte Castelo');
 INSERT INTO CLIENTE(nome, cpf, endereco, cidade) VALUES('Bernardo', '123.456.789-12', 'Rua brasil, Bariro Centro', 'Monte Castelo');
 INSERT INTO CLIENTE(nome, cpf, endereco, cidade) VALUES('Alfredo', '123.456.789-12', 'Rua das bergamotas, Bariro das bergabotas', 'Itapuca');
+
 		
 INSERT INTO CATEGORIA(codigo, descricao, nome) VALUES('codigo eletrodomestico', 'descricao eletrodomestico', 'Eletrodomestico');
 INSERT INTO CATEGORIA(codigo, descricao, nome) VALUES('Codigo alimento', 'descricao alimento', 'Alimento');
 
+
 INSERT INTO LOJA(cidade, cnpj, endereco, nome) VALUES('São Paulo', '99.999.999/9999-99', 'RUA SANTOS D.', 'ELETROBOM LTDA');
 INSERT INTO LOJA(cidade, cnpj, endereco, nome) VALUES('São Paulo', '99.999.999/2992-92', 'RUA Da conceição', 'BOM ALIMENTO LTDA');
+
 
 INSERT INTO VENDEDOR(comicao, nome, salario, loja_id) VALUES('10.0', 'João Cleber Nascimento', '3000.0', '1');
 INSERT INTO VENDEDOR(comicao, nome, salario, loja_id) VALUES('15.0', 'Jorel Camargo Santana', '5000.0', '1');
 INSERT INTO VENDEDOR(comicao, nome, salario, loja_id) VALUES('20.0', 'Maria Ines Pererira', '2000.0', '2');
 
+
 INSERT INTO PRODUTO(codigo, descricao, preco_unitario, quantidade, categoria_id) VALUES('codigo produto 1', 'Torradeira 110/220 Torra Bem', '119.99', '1', '1');
 INSERT INTO PRODUTO(codigo, descricao, preco_unitario, quantidade, categoria_id) VALUES('codigo produto 2', 'Smart TV LG', '1500', '1', '1');
 INSERT INTO PRODUTO(codigo, descricao, preco_unitario, quantidade, categoria_id) VALUES('codigo produto 3', 'Aveia de trigo', '8.99', '5', '2');
 
-INSERT INTO PEDIDO_VENDA(data_pedido, valor_total, cliente_id, vendedor_id) VALUES('2020-02-02 08:10:07', '123456789', '1', '1');
 
- 
+INSERT INTO PEDIDO_VENDA(data_pedido, valor_total, cliente_id, vendedor_id) VALUES('2020-02-02 08:10:07', '123456789', '1', '1');
+INSERT INTO PEDIDO_VENDA(data_pedido, valor_total, cliente_id, vendedor_id) VALUES('2020-10-02 08:10:07', '999', '1', '2');
+
+INSERT INTO PEDIDO_VENDA_LIST_PRODUTOS(pedido_venda_id, list_produtos_id) VALUES('1', '2');
+INSERT INTO PEDIDO_VENDA_LIST_PRODUTOS(pedido_venda_id, list_produtos_id) VALUES('1', '1');
+INSERT INTO PEDIDO_VENDA_LIST_PRODUTOS(pedido_venda_id, list_produtos_id) VALUES('1', '3');
+INSERT INTO PEDIDO_VENDA_LIST_PRODUTOS(pedido_venda_id, list_produtos_id) VALUES('2', '3');
